98117a1f450e2027460b16e8d1fac1903a1eaa99
Tamas@fedora <tsmahajcsiks@avaya.com>
2024-05-27 22:46:59 +0200

    add R and add_articulation_point function

---
+++ R/add_articulation_point.R
@@ -0,0 +1,9 @@
+#' Adds articulation point to an igraph graph
+#' Returns a graph
+require("igraph")
+
+add_articulation_point <- function(graph) {
+  V(graph)[articulation_points(graph)]$articulation_point <- "*"
+  V(graph)$articulation_point[is.na(V(graph)$articulation_point)] <- ""
+  return(graph)
+}
