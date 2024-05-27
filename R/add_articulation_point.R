fdeac41adcb31a72aeda35c0776ea1113a47e31c
Tamas@fedora <tsmahajcsiks@avaya.com>
2024-05-27 22:55:27 +0200

    update roxygen2 docs

--- R/add_articulation_point.R
+++ R/add_articulation_point.R
@@ -1,7 +1,7 @@
-#' Adds articulation point to an igraph graph
-#' Returns a graph
 require("igraph")
 
+#' Adds articulation point to an igraph graph
+#' Returns a graph
 add_articulation_point <- function(graph) {
   V(graph)[articulation_points(graph)]$articulation_point <- "*"
   V(graph)$articulation_point[is.na(V(graph)$articulation_point)] <- ""
